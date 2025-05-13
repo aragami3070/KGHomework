cdf8087dbcd8779287d3dda181eeda6a0f81b95b
aragami3070 <smirnov17612@gmail.com>
2025-05-13 16:50:26 +0400

    Add: initial Smirnov251HW7

---
+++ Smirnov251HW7/Smirnov251HW7/Main.cpp
@@ -0,0 +1,13 @@
+#define GLM_ENABLE_EXPERIMENTAL
+#include <glad\glad.h>
+#include <GLFW\glfw3.h>
+#include <glm\glm.hpp>
+#include <glm\gtx\transform.hpp>
+#include <glm\gtc\type_ptr.hpp>
+#include <iostream>
+
+int main() {
+	std::cout << "Test" << std::endl;
+    
+	return 0;
+}
